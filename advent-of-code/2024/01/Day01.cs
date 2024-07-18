800cd09860922d5d52e820d6ac2abf0fc07c2630
Wesley Van Koeckhoven <wvk@outlook.be>
2024-07-18 23:14:42 +0200

    setup framework

---
+++ advent-of-code/2024/01/Day01.cs
@@ -0,0 +1,11 @@
+namespace advent_of_code._2024._01;
+
+[TestClass]
+public class Day01
+{
+    [TestMethod]
+    public void Puzzle1()
+    {
+        throw new NotImplementedException();
+    }
+}
