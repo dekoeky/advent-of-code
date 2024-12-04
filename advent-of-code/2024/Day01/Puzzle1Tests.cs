a68257a131e00e08a173e95c6ca79435c24434be
Wesley Van Koeckhoven <wvk@outlook.be>
2024-12-04 17:13:46 +0100

    relocate

---
+++ advent-of-code/2024/Day01/Puzzle1Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace advent_of_code._2024.Day01;
+
+[TestClass]
+public class Puzzle1Tests
+{
+    [TestMethod]
+    public void Example()
+    {
+        //Arrange
+        var input = Inputs.Example;
+        InputParsing.ParseInput(input, out var list1, out var list2);
+
+        //Act
+        var result = Calculations.CalculateTotalDistance(list1, list2);
+
+        //Assert
+        Assert.AreEqual(11, result);
+    }
+
+    [TestMethod]
+    public void ActualPuzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle1;
+        InputParsing.ParseInput(input, out var list1, out var list2);
+
+        //Act
+        var result = Calculations.CalculateTotalDistance(list1, list2);
+
+        //Assert
+        Console.WriteLine($"Total Distance: {result}");
+    }
+}
---
+++ advent-of-code/2024/Day01/Puzzle2Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace advent_of_code._2024.Day01;
+
+[TestClass]
+public class Puzzle2Tests
+{
+    [TestMethod]
+    public void Example()
+    {
+        //Arrange
+        var input = Inputs.Example;
+        InputParsing.ParseInput(input, out var list1, out var list2);
+
+        //Act
+        var result = Calculations.CalculateSimilarityScore(list1, list2);
+
+        //Assert
+        Assert.AreEqual(31, result);
+    }
+
+    [TestMethod]
+    public void ActualPuzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle2;
+        InputParsing.ParseInput(input, out var list1, out var list2);
+
+        //Act
+        var result = Calculations.CalculateSimilarityScore(list1, list2);
+
+        //Assert
+        Console.WriteLine($"Similarity Score: {result}");
+    }
+}
