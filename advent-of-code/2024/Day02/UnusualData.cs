93102da3c6119dbccc94c6afff8dfcb9197556b6
Wesley Van Koeckhoven <wvk@outlook.be>
2024-12-04 18:17:21 +0100

    Part 1

---
+++ advent-of-code/2024/Day02/UnusualData.cs
@@ -0,0 +1,17 @@
+﻿namespace advent_of_code._2024.Day02;
+
+internal class UnusualData
+{
+    static readonly char[] lineChars = ['\r', '\n'];
+    public static UnusualData Parse(string input)
+    {
+        var lines = input.Split(lineChars, StringSplitOptions.RemoveEmptyEntries);
+
+        return new UnusualData
+        {
+            Reports = lines.Select(Report.Parse).ToArray()
+        };
+    }
+
+    public required Report[] Reports { get; set; }
+}
