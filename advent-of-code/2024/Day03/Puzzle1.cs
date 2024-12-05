60d8f5c31f80a132e610aa9fb1a3a1f0660fcc6e
Wesley Van Koeckhoven <wvk@outlook.be>
2024-12-05 18:03:27 +0100

    part1

---
+++ advent-of-code/2024/Day03/Puzzle1.cs
@@ -0,0 +1,31 @@
+﻿namespace advent_of_code._2024.Day03;
+
+[TestClass]
+public class Puzzle1
+{
+    [TestMethod]
+    public void Example()
+    {
+        //Arrange
+        var input = Input.Example;
+
+        //Act
+        var count = Calculations.SumOfMultiplications(input);
+
+        //Assert
+        Assert.AreEqual(161, count);
+    }
+
+    [TestMethod]
+    public void Puzzle()
+    {
+        //Arrange
+        var input = Input.Puzzle1;
+
+        //Act
+        var count = Calculations.SumOfMultiplications(input);
+
+        //Assert
+        Console.WriteLine(count);
+    }
+}
