d49ac74e43273c32c77612200776b7494375d1f5
Wesley VAN KOECKHOVEN <VT6109@equans.com>
2024-12-09 18:18:27 +0100

    templates

---
+++ advent-of-code/_Templates/Inputs.cs
@@ -0,0 +1,12 @@
+﻿namespace advent_of_code._Templates;
+
+public static class Inputs
+{
+    public const string Example = """
+
+                                  """;
+
+    public const string Puzzle = """
+
+                                 """;
+}
---
+++ advent-of-code/_Templates/Puzzle.cs
@@ -0,0 +1,49 @@
+﻿namespace advent_of_code._Templates;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Example1()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+
+    [TestMethod]
+    public void Puzzle1()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+
+    [TestMethod]
+    public void Example2()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+}
