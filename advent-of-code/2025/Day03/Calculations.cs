74c993f08e9735f695789e09fcc9d6321dd6a7e4
Wesley Van Koeckhoven <wvk@outlook.be>
2025-12-03 11:24:51 +0100

    Puzzle 2025 day 3 part 1

---
+++ advent-of-code/2025/Day03/Calculations.cs
@@ -0,0 +1,33 @@
+﻿namespace advent_of_code._2025.Day03;
+
+public static class Calculations
+{
+    private static readonly char[] newlineChars = ['\r', '\n'];
+
+    public static int CalculateJoltageSum(string input)
+    {
+        var lines = input.Split(newlineChars);
+
+        return lines.Select(FindLargestJoltage).Sum();
+    }
+
+    public static int FindLargestJoltage(string input)
+    {
+        var numbers = input.Select(c => c - '0').ToArray();
+
+        var max = (Tens: 0, Ones: 0);
+
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            var lastValue = i == numbers.Length - 1;
+
+            var value = numbers[i];
+            if (value > max.Tens && !lastValue)
+                max = (value, 0);
+            else if (value > max.Ones)
+                max.Ones = value;
+        }
+
+        return max.Tens * 10 + max.Ones;
+    }
+}
---
+++ advent-of-code/2025/Day03/Puzzle.cs
@@ -0,0 +1,53 @@
+﻿namespace advent_of_code._2025.Day03;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Example1()
+    {
+        //Arrange
+        var input = Inputs.Example;
+
+        //Act
+        var sum = Calculations.CalculateJoltageSum(input);
+
+        //Assert
+        Assert.AreEqual(357, sum);
+    }
+
+    [TestMethod]
+    public void Puzzle1()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var sum = Calculations.CalculateJoltageSum(input);
+
+        //Assert
+        Console.WriteLine($"Sum of Max Joltages: {sum}");
+    }
+
+    [TestMethod]
+    public void Example2()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+}
