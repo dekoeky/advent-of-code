26955338e848195ea8e0acc80bf22590e702ba9f
Wesley Van Koeckhoven <wvk@outlook.be>
2025-12-06 21:52:23 +0100

    puzzle 2025 Day06 Part1

---
+++ advent-of-code/2025/Day06/Calculations.cs
@@ -0,0 +1,38 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code._2025.Day06;
+
+public static class Calculations
+{
+    public static long SumOfOperations(string input)
+    {
+        var lines = SplitOn.NewLines(input);
+
+        var operations = ParseLine(lines.Last(), ToOperator);
+        var values = ParseLine(lines.First(), long.Parse);
+
+        for (var i = 1; i < lines.Length - 1; i++)
+        {
+            var vv = ParseLine(lines[i], long.Parse);
+            for (var j = 0; j < vv.Length; j++)
+            {
+                var operation = operations[j];
+                values[j] = operation(values[j], vv[j]);
+            }
+        }
+
+        return values.Sum();
+    }
+
+    private static T[] ParseLine<T>(string line, Func<string, T> x) =>
+        line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(x)
+            .ToArray();
+
+    private static Func<long, long, long> ToOperator(string input) => input switch
+    {
+        "+" => (a, b) => a + b,
+        "*" => (a, b) => a * b,
+        _ => throw new InvalidOperationException()
+    };
+}
---
+++ advent-of-code/2025/Day06/Puzzle.cs
@@ -0,0 +1,57 @@
+﻿namespace advent_of_code._2025.Day06;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Example1()
+    {
+        //Arrange
+        var input = Inputs.Example;
+
+        //Act
+        var result = Calculations.SumOfOperations(input);
+
+        //Assert
+        Assert.AreEqual(4277556, result);
+    }
+
+    [TestMethod]
+    public void Puzzle1()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.SumOfOperations(input);
+
+        //Assert
+        Console.WriteLine($"Result: {result}");
+    }
+
+    [TestMethod]
+    public void Example2()
+    {
+        //Arrange
+        var input = Inputs.Example;
+
+        //Act
+        var result = Calculations.SumOfOperations(input);
+
+        //Assert
+        Assert.AreEqual(00000000, result);
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.SumOfOperations(input);
+
+        //Assert
+        Console.WriteLine($"Result: {result}");
+    }
+}
