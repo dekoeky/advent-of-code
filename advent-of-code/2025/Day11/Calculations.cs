585afa574cc92e6b921e50a7ed4a8bb48191090f
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-02 17:34:12 +0100

    Day 11 Part 1

---
+++ advent-of-code/2025/Day11/Calculations.cs
@@ -0,0 +1,43 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code._2025.Day11;
+
+internal static class Calculations
+{
+    public static Dictionary<string, string[]> Parse(string input)
+        => SplitOn.NewLines(input)
+        .Select(line => line.Split(": "))
+        .ToDictionary(parts => parts[0], parts => parts[1].Split(' '));
+
+    public static int Perform(string input)
+    {
+        var data = Parse(input);
+        data.Add("out", []);
+
+        var memo = new Dictionary<string, int>();
+        int result = CountPaths("you", data, memo);
+
+        return result;
+    }
+
+    public static int Perform2(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static int CountPaths(string node, Dictionary<string, string[]> graph, Dictionary<string, int> memo)
+    {
+        if (node == "out")
+            return 1;
+
+        if (memo.TryGetValue(node, out int cached))
+            return cached;
+
+        int total = 0;
+        foreach (var child in graph[node])
+            total += CountPaths(child, graph, memo);
+
+        memo[node] = total;
+        return total;
+    }
+}
---
+++ advent-of-code/2025/Day11/Puzzle.cs
@@ -0,0 +1,58 @@
+﻿namespace advent_of_code._2025.Day11;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Example1()
+    {
+        //Arrange
+        var input = Inputs.Example;
+
+        //Act
+        var result = Calculations.Perform(input);
+
+        //Assert
+        Assert.AreEqual(5, result);
+    }
+
+    [TestMethod]
+    public void Puzzle1()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.Perform(input);
+
+        //Assert
+        Console.WriteLine($"Result: {result}");
+        Assert.AreEqual(423, result);
+    }
+
+    [TestMethod]
+    public void Example2()
+    {
+        //Arrange
+        var input = Inputs.Example;
+
+        //Act
+        var result = Calculations.Perform2(input);
+
+        //Assert
+        Assert.AreEqual(2, result);
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.Perform2(input);
+
+        //Assert
+        Console.WriteLine($"Result: {result}");
+    }
+}
