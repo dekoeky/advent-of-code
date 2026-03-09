a24f19d31ba6012f3662665e37932c7ff6b69d98
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-09 23:38:34 +0100

    Year 2015 Day 05 Part 1

---
+++ advent-of-code/2015/Day05/Puzzle.cs
@@ -0,0 +1,62 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code._2015.Day05;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    [DataRow("ugknbfddgicrmopn", true)]
+    [DataRow("aaa", true)]
+    [DataRow("jchzalrnumimnmhp", false)]
+    [DataRow("haegwjzuvuyypxyu", false)]
+    [DataRow("dvszwmarrgswjxmb", false)]
+    public void Example1(string input, bool expected)
+    {
+        //Act
+        var isNice = input.IsNice();
+
+        //Assert
+        Assert.AreEqual(expected, isNice);
+    }
+
+    [TestMethod]
+    public void Puzzle1()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var inputs = SplitOn.NewLines(input);
+
+        //Act
+        var result = inputs.Count(i => i.IsNice());
+
+        //Assert
+        Assert.AreEqual(258, result);
+    }
+
+    [TestMethod]
+    public void Example2()
+    {
+        //Arrange
+        var input = "";
+
+        //Act
+        var result = Calculations.Perform(input);
+
+        //Assert
+        Assert.AreEqual(00000000, result);
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.Perform(input);
+
+        //Assert
+        Console.WriteLine($"Result: {result}");
+    }
+}
---
+++ advent-of-code/2015/Day05/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace advent_of_code._2015.Day05;
+
+internal static class StringExtensions
+{
+    private const string vowels = "aeiou";
+    private static readonly string[] notAllowed = [
+        "ab",
+        "cd",
+        "pq",
+        "xy",
+        ];
+
+    extension(string input)
+    {
+        public bool IsNice()
+        {
+            if (notAllowed.Any(input.Contains))
+                return false;
+
+            int vowelCount = 0;
+            bool containsTwoSameLettersInARow = false;
+            char last = default;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+
+                if (vowels.Contains(c)) vowelCount++;
+
+                if (last == c) containsTwoSameLettersInARow = true; ;
+                last = c;
+            }
+
+            return vowelCount >= 3 && containsTwoSameLettersInARow;
+        }
+    }
+}
