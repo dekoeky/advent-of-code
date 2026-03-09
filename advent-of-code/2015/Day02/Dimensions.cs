b49336842599bff85dffe42e5c69122be5ee4ef7
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-09 22:25:26 +0100

    Year 2015 Day 02

---
+++ advent-of-code/2015/Day02/Dimensions.cs
@@ -0,0 +1,49 @@
+﻿namespace advent_of_code._2015.Day02;
+
+internal readonly record struct Dimensions(int L, int W, int H)
+{
+    public int PaperNeeded()
+    {
+        checked
+        {
+            int[] surfaces =
+            [
+                L * W,
+                W * H,
+                L * H,
+            ];
+
+            var slack = surfaces.Min();
+
+            return surfaces.Sum() * 2 + slack;
+        }
+    }
+
+    public int RibbonNeeded()
+    {
+        checked
+        {
+            int[] halfPerimeters = [
+                (L + W),
+                (W + H),
+                (L + H)
+                ];
+            var minPerimeter = halfPerimeters.Min() * 2;
+
+            var bowLength = L * W * H;
+
+            return minPerimeter + bowLength;
+        }
+    }
+
+    public static Dimensions Parse(string input)
+    {
+        var parts = input.Split('x');
+
+        return new Dimensions(
+            int.Parse(parts[0]),
+            int.Parse(parts[1]),
+            int.Parse(parts[2]));
+    }
+    public override string ToString() => $"{L}x{W}x{H}";
+}
---
+++ advent-of-code/2015/Day02/Puzzle.cs
@@ -0,0 +1,68 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code._2015.Day02;
+
+/// <summary>
+/// Solves puzzle <see href="https://adventofcode.com/2015/day/2"/>
+/// </summary>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    [DataRow(58, "2x3x4")]
+    [DataRow(43, "1x1x10")]
+    public void Part1Examples(int expectedSquareFeet, string input)
+    {
+        //Arrange
+        var dimension = Dimensions.Parse(input);
+
+        //Act
+        var result = dimension.PaperNeeded();
+
+        //Assert
+        Assert.AreEqual(expectedSquareFeet, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var dimensions = SplitOn.NewLines(input).Select(Dimensions.Parse);
+
+        //Act
+        var result = dimensions.Sum(d => d.PaperNeeded());
+
+        //Assert
+        Assert.AreEqual(1586300, result);
+    }
+
+    [TestMethod]
+    [DataRow(34, "2x3x4")]
+    [DataRow(14, "1x1x10")]
+    public void Part2Examples(int expected, string input)
+    {
+        //Arrange
+        var dimensions = Dimensions.Parse(input);
+
+        //Act
+        var result = dimensions.RibbonNeeded();
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var dimensions = SplitOn.NewLines(input).Select(Dimensions.Parse);
+
+        //Act
+        var result = dimensions.Sum(d => d.RibbonNeeded());
+
+        //Assert
+        Assert.AreEqual(3737498, result);
+    }
+}
