9555e3649aeb9c8a135da8a85aacaa925a701a3a
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-10 20:36:45 +0100

    Year 2015 Day 08

---
+++ advent-of-code/2015/Day08/Calculations.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace advent_of_code._2015.Day08;
+
+internal static class Calculations
+{
+    public static int Delta1(string input) => GetCodeLength(input) - GetStringLength(input);
+
+    public static int GetCodeLength(string input) => input.Length;
+
+    public static int GetStringLength(string input) => GetUnEscaped(input).Length;
+
+    private static string GetUnEscaped(string input) => Regex.Unescape(input[1..^1]);
+
+    public static int Delta2(string input) => GetEncodedLength(input) - GetCodeLength(input);
+
+    public static int GetEncodedLength(string input) => GetEncoded(input).Length;
+    public static string GetEncoded(string input)
+    {
+        var encoded = Regex.Escape(input);
+
+        encoded = encoded.Replace("\"", "\\\"");
+        encoded = $"\"{encoded}\"";
+
+        Debug.WriteLine($"{input} -> {encoded}");
+
+        return encoded;
+    }
+}
---
+++ advent-of-code/2015/Day08/Puzzle.cs
@@ -0,0 +1,103 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code._2015.Day08;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    [DataRow("""
+             ""
+             """, 2, 0)]
+    [DataRow("""
+             "abc"
+             """, 5, 3)]
+    [DataRow("""
+             "aaa\"aaa"
+             """, 10, 7)]
+    [DataRow("""
+             "\x27"
+             """, 6, 1)]
+    public void Part1Examples(string input, int expectedCodeLength, int expectedStringLength)
+    {
+        //Act
+        var codeLength = input.Length;
+        int stringLength = Calculations.GetStringLength(input);
+
+        //Assert
+        Assert.AreEqual(expectedCodeLength, codeLength);
+        Assert.AreEqual(expectedStringLength, stringLength);
+    }
+
+    [TestMethod]
+    [DataRow("""
+             ""
+             """, 2 - 0)]
+    [DataRow("""
+             "abc"
+             """, 5 - 3)]
+    [DataRow("""
+             "aaa\"aaa"
+             """, 10 - 7)]
+    [DataRow("""
+             "\x27"
+             """, 6 - 1)]
+    public void Part1Examples(string input, int expectedDelta)
+    {
+        //Act;
+        int delta = Calculations.Delta1(input);
+
+        //Assert
+        Assert.AreEqual(expectedDelta, delta);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var lines = SplitOn.NewLines(input);
+
+        //Act
+        int delta = lines.Sum(Calculations.Delta1);
+
+        //Assert
+        Assert.AreEqual(1371, delta);
+    }
+
+    [TestMethod]
+    [DataRow("""
+             ""
+             """, 6 - 2)]
+    [DataRow("""
+             "abc"
+             """, 9 - 5)]
+    [DataRow("""
+             "aaa\"aaa"
+             """, 16 - 10)]
+    [DataRow("""
+             "\x27"
+             """, 11 - 6)]
+    public void Part2Example(string input, int expectedEncodedLength)
+    {
+        //Act
+        var encodedLength = Calculations.Delta2(input);
+
+        //Assert
+        Assert.AreEqual(expectedEncodedLength, encodedLength);
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var lines = SplitOn.NewLines(input);
+
+        //Act
+        int delta = lines.Sum(Calculations.Delta2);
+
+        //Assert
+        Assert.AreEqual(2117, delta);
+    }
+}
