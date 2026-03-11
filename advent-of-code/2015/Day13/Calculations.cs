36b065c1ab052d63cbca52fa13034e32dc5ea955
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-11 01:09:31 +0100

    Year 2015 Day 13

---
+++ advent-of-code/2015/Day13/Calculations.cs
@@ -0,0 +1,49 @@
+﻿using advent_of_code.Helpers;
+using System.Diagnostics;
+
+namespace advent_of_code._2015.Day13;
+
+internal static class Calculations
+{
+    public static int OptimalSeatingHappiness(string input, bool includeMe = false)
+    {
+        var rules = SplitOn.NewLines(input).Select(Rule.Parse).ToArray();
+        //foreach (var rule in rules) Debug.WriteLine(rule.ToParsableString());
+
+        var a = rules.Select(r => r.Name);
+        var b = rules.Select(r => r.Neighbor);
+        var people = a.Concat(b).Distinct().ToList();
+        if (includeMe) people.Add("me");
+
+        return GetCombinationHapinesses(rules, people).Max();
+    }
+
+    private static IEnumerable<int> GetCombinationHapinesses(Rule[] rules, IReadOnlyCollection<string> people)
+    {
+        foreach (var seatingArrangement in people.AllPermutations())
+            yield return CalclateHappiness(rules, seatingArrangement);
+    }
+
+    private static int CalclateHappiness(Rule[] rules, IReadOnlyCollection<string> seatingArrangement)
+    {
+        var happiness = 0;
+
+        for (int i = 0; i < seatingArrangement.Count; i++)
+        {
+            var personBeingChecked = seatingArrangement.ElementAt(i);
+            string[] neighbors = [
+                seatingArrangement.ElementAt ((seatingArrangement .Count + i + 1) % seatingArrangement.Count ),
+                seatingArrangement.ElementAt ((seatingArrangement .Count + i - 1) % seatingArrangement.Count ),
+                ];
+            var applicableRules = rules.Where(r => r.Name == personBeingChecked && neighbors.Contains(r.Neighbor));
+
+            foreach (var rule in applicableRules)
+            {
+                Debug.WriteLine(rule.GetApplicableText());
+                happiness += rule.HappinessGain;
+            }
+        }
+
+        return happiness;
+    }
+}
---
+++ advent-of-code/2015/Day13/Puzzle.cs
@@ -0,0 +1,44 @@
+﻿namespace advent_of_code._2015.Day13;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Example1()
+    {
+        //Arrange
+        var input = Inputs.Example;
+
+        //Act
+        var result = Calculations.OptimalSeatingHappiness(input);
+
+        //Assert
+        Assert.AreEqual(330, result);
+    }
+
+    [TestMethod]
+    public void Puzzle1()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.OptimalSeatingHappiness(input);
+
+        //Assert
+        Assert.AreEqual(618, result);
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.OptimalSeatingHappiness(input, true);
+
+        //Assert
+        Assert.AreEqual(601, result);
+    }
+}
