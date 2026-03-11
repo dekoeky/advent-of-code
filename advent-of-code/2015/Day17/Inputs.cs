a93d07706482656879e87c1595750632687c42f5
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-12 00:59:20 +0100

    Year 2015 Day 17

---
+++ advent-of-code/2015/Day17/Inputs.cs
@@ -0,0 +1,30 @@
+﻿namespace advent_of_code._Templates.Day17;
+
+public static class Inputs
+{
+
+
+    public const string Puzzle =
+        """
+        50
+        44
+        11
+        49
+        42
+        46
+        18
+        32
+        26
+        40
+        21
+        7
+        18
+        43
+        10
+        47
+        36
+        24
+        22
+        40
+        """;
+}
---
+++ advent-of-code/2015/Day17/Puzzle.cs
@@ -0,0 +1,37 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code._Templates.Day17;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Puzzle1()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        int totalLiters = 150;
+
+        //Act
+        var containerSizes = SplitOn.NewLines(input).Select(int.Parse).ToArray();
+        var result = ContainerCombinations.SolvePart1(totalLiters, containerSizes);
+
+        //Assert
+        Assert.AreEqual(654, result);
+    }
+
+    [TestMethod]
+    public void Puzzle2()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        int totalLiters = 150;
+
+        //Act
+        var containerSizes = SplitOn.NewLines(input).Select(int.Parse).ToArray();
+        var result = ContainerCombinations.SolvePart2(totalLiters, containerSizes);
+
+        //Assert
+        Assert.AreEqual(57, result);
+    }
+}
