da57c8e3cf13def43e3830baa872491c9d165312
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-12 20:57:44 +0100

    Year 2015 Day20

---
+++ advent-of-code/2015/Day20/Calculations.cs
@@ -0,0 +1,40 @@
+namespace advent_of_code._Templates.Day20;
+
+internal static class Calculations
+{
+    public static int Part1(int target, int visits)
+    {
+        int upperBound = target / visits;
+        var houses = new int[upperBound + 1];
+
+        for (int elf = 1; elf <= upperBound; elf++)
+            for (int house = elf; house <= upperBound; house += elf)
+                houses[house] += elf * visits;
+
+        for (int i = 1; i <= upperBound; i++)
+            if (houses[i] >= target)
+                return i;
+
+        return -1;
+    }
+
+    public static int Part2(int target, int visits, int limit)
+    {
+        int upperBound = target / visits;
+        var houses = new int[upperBound + 1];
+
+        for (int elf = 1; elf <= upperBound; elf++)
+        {
+            int maxHouse = Math.Min(upperBound, elf * limit);
+
+            for (int house = elf; house <= maxHouse; house += elf)
+                houses[house] += elf * visits;
+        }
+
+        for (int i = 1; i <= upperBound; i++)
+            if (houses[i] >= target)
+                return i;
+
+        return -1;
+    }
+}
---
+++ advent-of-code/2015/Day20/Puzzle.cs
@@ -0,0 +1,34 @@
+namespace advent_of_code._Templates.Day20;
+
+[TestClass]
+public class Puzzle
+{
+    private const int Input = 36000000;
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        int visits = 10;
+
+        //Act
+        var result = Calculations.Part1(Input, visits);
+
+        //Assert
+        Assert.AreEqual(831600, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var visits = 11;
+        var limit = 50;
+
+        //Act
+        var result = Calculations.Part2(Input, visits, limit);
+
+        //Assert
+        Assert.AreEqual(884520, result);
+    }
+}
