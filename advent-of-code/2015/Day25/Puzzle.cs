765196b55ab1e07df6ed9f95ec9ed97348dc03c1
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-16 00:46:17 +0100

    year 2015 day 25 part 1

---
+++ advent-of-code/2015/Day25/Puzzle.cs
@@ -0,0 +1,15 @@
+namespace advent_of_code._2015.Day25;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Act
+        var result = Calculations.Solve(2947, 3029);
+
+        //Assert
+        Assert.AreEqual(19980801L, result);
+    }
+}
