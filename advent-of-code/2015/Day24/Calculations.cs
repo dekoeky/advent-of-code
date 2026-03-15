327c3e1f18bf9eec3a68bd1e3de136f9fc037022
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-16 00:34:01 +0100

    year 2015 day 24

---
+++ advent-of-code/2015/Day24/Calculations.cs
@@ -0,0 +1,56 @@
+namespace advent_of_code._2015.Day24;
+
+public static class Calculations
+{
+    public static long Solve(int[] packages, int groups)
+    {
+        // If all groups need to weigh the same, we should be able to get a target weight for each group
+        int total = packages.Sum();
+        int target = total / groups;
+
+        // Sort heavy to light, to find a solution faster
+        var sorted = packages.OrderByDescending(x => x).ToArray();
+
+        // Try small group 1 first, 
+        for (int size = 1; size <= sorted.Length; size++)
+        {
+            var validGroups =
+                Combinations(sorted, size)
+                .Where(c => c.Sum() == target)
+                .ToList();
+
+            if (validGroups.Count == 0)
+                continue;
+
+            return validGroups.Min(c => c.Aggregate(1L, (acc, v) => acc * v));
+        }
+
+        throw new Exception("No valid group found.");
+    }
+
+    private static IEnumerable<List<int>> Combinations(int[] arr, int size)
+    {
+        int n = arr.Length;
+        int[] idx = [.. Enumerable.Range(0, size)];
+
+        while (true)
+        {
+            var combo = new List<int>(size);
+            for (int i = 0; i < size; i++)
+                combo.Add(arr[idx[i]]);
+            yield return combo;
+
+            int k;
+            for (k = size - 1; k >= 0; k--)
+                if (idx[k] != k + n - size)
+                    break;
+
+            if (k < 0)
+                yield break;
+
+            idx[k]++;
+            for (int j = k + 1; j < size; j++)
+                idx[j] = idx[j - 1] + 1;
+        }
+    }
+}
---
+++ advent-of-code/2015/Day24/Puzzle.cs
@@ -0,0 +1,61 @@
+namespace advent_of_code._2015.Day24;
+
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        //Arrange
+        var input = Inputs.Example;
+        var weights = Weights.Parse(input);
+
+        //Act
+        var result = Calculations.Solve(weights, 3);
+
+        //Assert
+        Assert.AreEqual(99, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var weights = Weights.Parse(input);
+
+        //Act
+        var result = Calculations.Solve(weights, 3);
+
+        //Assert
+        Assert.AreEqual(10723906903L, result);
+    }
+
+    [TestMethod]
+    public void Part2Example()
+    {
+        //Arrange
+        var input = Inputs.Example;
+        var weights = Weights.Parse(input);
+
+        //Act
+        var result = Calculations.Solve(weights, 4);
+
+        //Assert
+        Assert.AreEqual(44, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var weights = Weights.Parse(input);
+
+        //Act
+        var result = Calculations.Solve(weights, 4);
+
+        //Assert
+        Assert.AreEqual(74850409L, result);
+    }
+}
