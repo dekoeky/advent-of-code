9f7f9dc72cc29b232ed740889956f9a4e1a0cf60
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-16 22:43:22 +0100

    fix warnings

--- advent-of-code/2015/Day14/ReindeerInfo.cs
+++ advent-of-code/2015/Day14/ReindeerInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 namespace advent_of_code._2015.Day14;
 
@@ -6,11 +6,11 @@ internal partial record ReindeerInfo(string Name, int Speed, int FlyTime, int Re
 {
     [GeneratedRegex("^([A-Za-z]+) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds\\.$")]
     private static partial Regex GetRegex();
-    private static readonly Regex regex = GetRegex();
+    private static readonly Regex Regex = GetRegex();
 
     public static ReindeerInfo Parse(string input)
     {
-        var m = regex.Match(input);
+        var m = Regex.Match(input);
 
         var name = m.Groups[1].Value;
         var speed = int.Parse(m.Groups[2].Value);
@@ -25,18 +25,16 @@ internal partial record ReindeerInfo(string Name, int Speed, int FlyTime, int Re
 
     public int DistanceAfter(int time)
     {
-        var distance = 0;
-        var timeFlown = 0;
+        int timeFlown;
 
         // Calculate number of full cycles
         var fullCycles = time / CycleTime;
         var remainingTime = time - fullCycles * CycleTime;
 
-        // Remaining time >FlyTime but <CycleTime:
-        if (remainingTime >= FlyTime)
-            timeFlown = (fullCycles + 1) * FlyTime;
-        else
-            timeFlown = fullCycles * FlyTime + remainingTime;
+        timeFlown = remainingTime >= FlyTime
+            // Remaining time >FlyTime but <CycleTime:
+            ? (fullCycles + 1) * FlyTime
+            : fullCycles * FlyTime + remainingTime;
 
         return timeFlown * Speed;
     }
