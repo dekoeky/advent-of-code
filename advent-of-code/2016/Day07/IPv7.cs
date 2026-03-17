4780f2fadb8363581dfb640d10f16b494091fb73
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-17 20:35:03 +0100

    year 2016 day 7

---
+++ advent-of-code/2016/Day07/IPv7.cs
@@ -0,0 +1,135 @@
+namespace advent_of_code._2016.Day07;
+
+internal static class IPv7
+{
+    public static int CountTlsSupportingIps(ReadOnlySpan<char> lines)
+    {
+        int count = 0;
+
+        foreach (var line in lines.EnumerateLines())
+            if (SupportsTls(line))
+                count++;
+
+        return count;
+    }
+
+    public static int CountSslSupportingIps(ReadOnlySpan<char> lines)
+    {
+        int count = 0;
+
+        foreach (var line in lines.EnumerateLines())
+            if (SupportsSsl(line))
+                count++;
+
+        return count;
+    }
+
+    public static bool SupportsTls(ReadOnlySpan<char> input)
+    {
+        bool hasABBA = false;
+        bool insideBrackets = false;
+
+        for (var i = 0; i <= input.Length - 4; i++)
+        {
+            if (input[i] == '[')
+            {
+                insideBrackets = true;
+                continue;
+            }
+            if (input[i] == ']')
+            {
+                insideBrackets = false;
+                continue;
+            }
+
+            if (input[i + 1] is '[' or ']')
+                continue;
+
+            if (input[i] != input[i + 3])
+                continue;
+
+            if (input[i + 1] != input[i + 2])
+                continue;
+
+            // AAAA is not valid, must be ABBA
+            if (input[i] == input[i + 1])
+                continue;
+
+            // ABBA pattern inside brackets, indicates TLS is not supported
+            if (insideBrackets)
+            {
+                return false;
+            }
+
+            // We now have ABBA pattern
+            hasABBA = true;
+
+            // We need to check the rest of the input, to validate no ABBA pattern in brackets
+        }
+
+        return hasABBA;
+    }
+
+    public static bool SupportsSsl(ReadOnlySpan<char> input)
+    {
+        Span<(char A, char B)> abas = stackalloc (char, char)[32];
+        Span<(char A, char B)> babs = stackalloc (char, char)[32];
+        int abaCount = 0;
+        int babCount = 0;
+
+        bool insideBrackets = false;
+
+        for (int i = 0; i <= input.Length - 3; i++)
+        {
+            char c = input[i];
+
+            if (c == '[')
+            {
+                insideBrackets = true;
+                continue;
+            }
+            if (c == ']')
+            {
+                insideBrackets = false;
+                continue;
+            }
+
+            // ABA = A B A, A != B
+            char a = input[i];
+            char b = input[i + 1];
+            char a2 = input[i + 2];
+
+            if (b == '[' || b == ']') continue;
+            if (a != a2) continue;
+            if (a == b) continue;
+
+            // ABA gevonden
+            if (!insideBrackets)
+            {
+                // ABA buiten brackets → sla op als (A,B)
+                if (abaCount < abas.Length)
+                    abas[abaCount++] = (a, b);
+            }
+            else
+            {
+                // BAB binnen brackets → sla op als (B,A)
+                if (babCount < babs.Length)
+                    babs[babCount++] = (b, a);
+            }
+        }
+
+        // Check of er een ABA buiten brackets is waarvoor een BAB binnen brackets bestaat
+        for (int i = 0; i < abaCount; i++)
+        {
+            var aba = abas[i];
+            for (int j = 0; j < babCount; j++)
+            {
+                var bab = babs[j];
+                if (aba.A == bab.A && aba.B == bab.B)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ advent-of-code/2016/Day07/Puzzle.cs
@@ -0,0 +1,63 @@
+namespace advent_of_code._2016.Day07;
+
+/// <summary>
+/// Year 2016 Day 07 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2016/day/7"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    [DataRow("abba[mnop]qrst", true)]
+    [DataRow("abcd[bddb]xyyx", false)]
+    [DataRow("aaaa[qwer]tyui", false)]
+    [DataRow("ioxxoj[asdfgh]zxcvbn", true)]
+    public void Part1Example(string input, bool expectedSupportsTls)
+    {
+        //Act
+        var result = IPv7.SupportsTls(input);
+
+        //Assert
+        Assert.AreEqual(expectedSupportsTls, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = IPv7.CountTlsSupportingIps(input);
+
+        //Assert
+        Assert.AreEqual(115, result);
+    }
+
+    [TestMethod]
+    [DataRow("aba[bab]xyz", true)]
+    [DataRow("xyx[xyx]xyx", false)]
+    [DataRow("aaa[kek]eke", true)]
+    [DataRow("zazbz[bzb]cdb", true)]
+    public void Part2Example(string input, bool expectedSupportsTls)
+    {
+        //Act
+        var result = IPv7.SupportsSsl(input);
+
+        //Assert
+        Assert.AreEqual(expectedSupportsTls, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = IPv7.CountSslSupportingIps(input);
+
+        //Assert
+        Assert.AreEqual(231, result);
+    }
+}
