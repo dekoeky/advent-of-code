f2c4fb7b425b0c0c4e82919f785156d6b0042b47
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-25 17:33:04 +0100

    year 2016 day 09

---
+++ advent-of-code/2016/Day09/Calculations.cs
@@ -0,0 +1,106 @@
+using System.Text;
+
+namespace advent_of_code._2016.Day09;
+
+internal static class Calculations
+{
+    public static string DecompressV1(ReadOnlySpan<char> input)
+    {
+        var sb = new StringBuilder();
+        int i;
+        for (i = 0; i < input.Length; i++)
+        {
+            var c = input[i];
+
+            if (c == '(')
+            {
+                var (count, repeat) = ReadMarker(input);
+
+                var rest = input[(i + 1)..];
+
+                // Cant process pattern if end of input string
+                if (rest.Length < 1) break;
+
+                //// Should not repeat pattern if pattern starts with '('
+                //if (rest[0] == '(')
+                //{
+                //    var l = rest.IndexOf(')') + 1;
+                //    sb.Append(rest[..l]);
+                //    i += l;
+                //    continue;
+                //}
+
+                for (var r = 0; r < repeat; r++)
+                    sb.Append(rest[..count]);
+
+                // Skip the repeated pattern
+                i += count;
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+
+        (int count, int repeat) ReadMarker(ReadOnlySpan<char> input)
+        {
+            // Ignore '('
+            i++;
+
+            var index = input.Slice(i).IndexOf('x');
+            var count = int.Parse(input.Slice(i, index));
+
+            // Ignore 'x'
+            i += index + 1;
+            index = input.Slice(i).IndexOf(')');
+            var repeat = int.Parse(input.Slice(i, index));
+
+            // Set index to ')'
+            i += index;
+
+            return (count, repeat);
+        }
+    }
+
+    public static long DecompressV2(ReadOnlySpan<char> input)
+    {
+        long total = 0;
+
+        for (int i = 0; i < input.Length; i++)
+            if (input[i] == '(')
+            {
+                var (count, repeat, newIndex) = ReadMarker(input, i);
+                i = newIndex;
+
+                var segment = input.Slice(i + 1, count);
+                long innerLength = DecompressV2(segment);
+
+                total += innerLength * repeat;
+
+                i += count;
+            }
+            else
+            {
+                total++;
+            }
+
+        return total;
+    }
+
+    private static (int count, int repeat, int endIndex) ReadMarker(ReadOnlySpan<char> input, int i)
+    {
+        i++; // skip '('
+        var x = input[i..].IndexOf('x');
+        var count = int.Parse(input.Slice(i, x));
+
+        i += x + 1;
+        var end = input[i..].IndexOf(')');
+        var repeat = int.Parse(input.Slice(i, end));
+
+        var endIndex = i + end; // index of ')'
+
+        return (count, repeat, endIndex);
+    }
+}
---
+++ advent-of-code/2016/Day09/Puzzle.cs
@@ -0,0 +1,66 @@
+namespace advent_of_code._2016.Day09;
+
+/// <summary>
+/// Year 2016 Day 09 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2016/day/9"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    [DataRow("ADVENT", "ADVENT")]
+    [DataRow("A(1x5)BC", "ABBBBBC")]
+    [DataRow("(3x3)XYZ", "XYZXYZXYZ")]
+    [DataRow("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG")]
+    [DataRow("(6x1)(1x3)A", "(1x3)A")]
+    [DataRow("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY")]
+    public void Part1Examples(string input, string expected)
+    {
+        //Act
+        var result = Calculations.DecompressV1(input);
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.DecompressV1(input);
+        var count = result.Count(c => !char.IsWhiteSpace(c));
+
+        //Assert
+        Assert.AreEqual(97714, count);
+    }
+
+    [TestMethod]
+    [DataRow("(3x3)XYZ", 9L)]
+    [DataRow("X(8x2)(3x3)ABCY", 20L)]
+    [DataRow("(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920L)]
+    [DataRow("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445L)]
+    public void Part2Examples(string input, long expected)
+    {
+        //Act
+        var result = Calculations.DecompressV2(input);
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+
+        //Act
+        var result = Calculations.DecompressV2(input);
+
+        //Assert
+        Assert.AreEqual(10762972461L, result);
+    }
+}
