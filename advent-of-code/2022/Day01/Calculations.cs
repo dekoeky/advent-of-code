8379d5a78f204ce635d56487238e9a2bdecd05e0
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-02 22:57:00 +0200

    year 2022 day 01

---
+++ advent-of-code/2022/Day01/Calculations.cs
@@ -0,0 +1,25 @@
+using advent_of_code.Helpers;
+
+namespace advent_of_code._2022.Day01;
+
+internal static class Calculations
+{
+    public static int Part1(string input)
+    {
+        var inputPerElf = SplitOn.EmptyLines(input);
+        var snacksPerElf = inputPerElf.Select(i => SplitOn.NewLines(i).Select(int.Parse).ToArray()).ToArray();
+
+        var mostCalories = snacksPerElf.Max(snacks => snacks.Sum());
+
+        return mostCalories;
+    }
+
+    public static int Part2(string input)
+    {
+        var inputPerElf = SplitOn.EmptyLines(input);
+        var snacksPerElf = inputPerElf.Select(i => SplitOn.NewLines(i).Select(int.Parse).ToArray()).ToArray();
+
+        var top3 = snacksPerElf.Select(snacks => snacks.Sum()).OrderByDescending(sum => sum).Take(3);
+        return top3.Sum();
+    }
+}
---
+++ advent-of-code/2022/Day01/Puzzle.cs
@@ -0,0 +1,61 @@
+namespace advent_of_code._2022.Day01;
+
+/// <summary>
+/// Year 2022 Day 01 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2022/day/1"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(24000, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(71502, result);
+    }
+
+    [TestMethod]
+    public void Part2Examples()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(45000, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(208191, result);
+    }
+}
