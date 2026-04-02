bce18ed2a2b6438d51e5b805571594e9160e6bfd
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-02 22:00:31 +0200

    year 2019 day 01

---
+++ advent-of-code/2019/Day01/Calculations.cs
@@ -0,0 +1,30 @@
+namespace advent_of_code._2019.Day01;
+
+internal static class Calculations
+{
+    public static int FuelRequired(int mass)
+    {
+        // Fuel required to launch a given module is based on its mass. 
+        // Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+
+        return (mass / 3) - 2;
+    }
+
+    public static int FuelRequiredRecursive(int mass)
+    {
+        var fuel = FuelRequired(mass);
+
+        var additionalMass = fuel;
+
+        while (additionalMass > 0)
+        {
+            var additionalFuel = Math.Max(0, FuelRequired(additionalMass));
+
+            fuel += additionalFuel;
+
+            additionalMass = additionalFuel;
+        }
+
+        return fuel;
+    }
+}
---
+++ advent-of-code/2019/Day01/Puzzle.cs
@@ -0,0 +1,65 @@
+using advent_of_code.Helpers;
+
+namespace advent_of_code._2019.Day01;
+
+/// <summary>
+/// Year 2019 Day 01 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2019/day/1"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    [DataRow(12, 2)]
+    [DataRow(14, 2)]
+    [DataRow(1969, 654)]
+    [DataRow(100756, 33583)]
+    public void Part1Examples(int mass, int expectedFuel)
+    {
+        //Act
+        var fuel = Calculations.FuelRequired(mass);
+
+        //Assert
+        Assert.AreEqual(expectedFuel, fuel);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var masses = SplitOn.NewLines(input).Select(int.Parse);
+
+        //Act
+        var fuel = masses.Sum(Calculations.FuelRequired);
+
+        //Assert
+        Assert.AreEqual(3323874, fuel);
+    }
+
+    [TestMethod]
+    [DataRow(100756, 50346)]
+    [DataRow(1969, 966)]
+    public void Part2Examples(int mass, int expected)
+    {
+        //Act
+        var result = Calculations.FuelRequiredRecursive(mass);
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var input = Inputs.Puzzle;
+        var masses = SplitOn.NewLines(input).Select(int.Parse);
+
+        //Act
+        var fuel = masses.Sum(Calculations.FuelRequiredRecursive);
+
+        //Assert
+        Assert.AreEqual(4982961, fuel);
+    }
+}
