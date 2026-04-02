0773e0436c59d930353fdce89a2552b2090e8ca6
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-02 22:24:13 +0200

    year 2020 day 01

---
+++ advent-of-code/2020/Day01/Puzzle.cs
@@ -0,0 +1,71 @@
+using advent_of_code.Helpers;
+
+namespace advent_of_code._2020.Day01;
+
+/// <summary>
+/// Year 2020 Day 01 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2020/day/1"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        // Arrange
+        var input = SplitOn.NewLines(Inputs.Example)
+            .Select(int.Parse)
+            .ToArray();
+
+        // Act
+        var result = Calculations.Part1(input, 2020);
+
+        // Assert
+        Assert.AreEqual(514579, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = SplitOn.NewLines(Inputs.Puzzle)
+            .Select(int.Parse)
+            .ToArray();
+
+        // Act
+        var result = Calculations.Part1(input, 2020);
+
+        // Assert
+        Assert.AreEqual(567171, result);
+    }
+
+    [TestMethod]
+    public void Part2Examples()
+    {
+        // Arrange
+        var input = SplitOn.NewLines(Inputs.Example)
+            .Select(int.Parse)
+            .ToArray();
+
+        // Act
+        var result = Calculations.Part2(input, 2020);
+
+        // Assert
+        Assert.AreEqual(241861950, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var input = SplitOn.NewLines(Inputs.Puzzle)
+            .Select(int.Parse)
+            .ToArray();
+
+        // Act
+        var result = Calculations.Part2(input, 2020);
+
+        // Assert
+        Assert.AreEqual(212428694, result);
+    }
+}
