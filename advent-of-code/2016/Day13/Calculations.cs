9d972a793c6c82148a4d4382e71e99870c249008
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-07 22:19:31 +0200

    year 2016 day 13

---
+++ advent-of-code/2016/Day13/Calculations.cs
@@ -0,0 +1,68 @@
+using System.Numerics;
+
+namespace advent_of_code._2016.Day13;
+
+internal static class Calculations
+{
+    public static int Part1(int fav, (int x, int y) target) => Execute(fav, target: target, maxSteps: null).Steps;
+
+    public static int Part2(int fav, int maxSteps) => Execute(fav, target: null, maxSteps: maxSteps).Seen;
+
+    private static (int Seen, int Steps) Execute(int fav, (int x, int y)? target, int? maxSteps)
+    {
+        var start = (x: 1, y: 1);
+        var queue = new Queue<((int x, int y) pos, int steps)>();
+        var seen = new HashSet<(int x, int y)>();
+        int steps = 0;
+        (int x, int y) pos;
+
+        queue.Enqueue((start, 0));
+        seen.Add(start);
+
+        while (queue.Count > 0)
+        {
+            (pos, steps) = queue.Dequeue();
+
+            if (pos == target)
+                break;
+
+            if (steps == maxSteps)
+                continue;
+
+            foreach (var n in Neighbors(pos))
+                if (!seen.Contains(n) && IsOpen(n.x, n.y, fav))
+                {
+                    seen.Add(n);
+                    queue.Enqueue((n, steps + 1));
+                }
+        }
+
+        return (seen.Count, steps);
+    }
+
+    private static bool IsOpen(int x, int y, int fav)
+    {
+        if (x < 0 || y < 0) return false;
+
+        long v = x * x
+            + 3 * x
+            + 2 * x * y
+            + y
+            + y * y
+            + fav;
+
+        // Count Bits
+        int bits = BitOperations.PopCount((uint)v);
+
+        // Open, when even
+        return bits % 2 == 0;
+    }
+
+    private static IEnumerable<(int x, int y)> Neighbors((int x, int y) p)
+    {
+        yield return (p.x + 1, p.y);
+        yield return (p.x - 1, p.y);
+        yield return (p.x, p.y + 1);
+        yield return (p.x, p.y - 1);
+    }
+}
---
+++ advent-of-code/2016/Day13/Puzzle.cs
@@ -0,0 +1,51 @@
+namespace advent_of_code._2016.Day13;
+
+/// <summary>
+/// Year 2016 Day 13 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2016/day/13"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        //Arrange
+        var fav = 10;
+        var target = (7, 4);
+
+        //Act
+        var result = Calculations.Part1(fav, target);
+
+        //Assert
+        Assert.AreEqual(11, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var fav = 1350;
+        var target = (31, 39);
+
+        //Act
+        var result = Calculations.Part1(fav, target);
+
+        //Assert
+        Assert.AreEqual(92, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var fav = 1350;
+        var maxSteps = 50;
+
+        //Act
+        var result = Calculations.Part2(fav, maxSteps);
+
+        //Assert
+        Assert.AreEqual(124, result);
+    }
+}
