ec92b8dc993c2c74ba2fa7761f894bffc82167e3
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-08 01:43:31 +0200

    year 2016 day 16

---
+++ advent-of-code/2016/Day16/Calculations.cs
@@ -0,0 +1,56 @@
+namespace advent_of_code._2016.Day16;
+
+internal static class Calculations
+{
+    public static string Execute(string initialState, int length)
+    {
+        var state = initialState;
+        while (state.Length < length)
+        {
+            state = UpdateState(state);
+            Debug.WriteLine($"State: {state}");
+        }
+        return CheckSum(state.Substring(0, length));
+    }
+
+    private static string UpdateState(string input)
+    {
+        var l = input.Length * 2 + 1;
+        char[] chars = new char[l];
+
+        input.CopyTo(chars);
+        chars[input.Length] = '0';
+
+        for (var i = 0; i < input.Length; i++)
+            chars[l - 1 - i] = input[i] == '1' ? '0' : '1';
+
+        return new string(chars);
+    }
+
+    private static string CheckSum(string input)
+    {
+        string checksum = input;
+        do
+        {
+            checksum = DoCheckSum(checksum);
+            Debug.WriteLine($"Checksum: {checksum}");
+        } while (checksum.Length % 2 == 0);
+
+        return checksum;
+    }
+
+    private static string DoCheckSum(string input)
+    {
+        var l = input.Length / 2;
+
+        var chars = new char[l];
+        Array.Fill(chars, '0');
+
+        for (var i = 0; i < l; i++)
+        {
+            chars[i] = input[i * 2] == input[i * 2 + 1] ? '1' : '0';
+        }
+
+        return new string(chars);
+    }
+}
---
+++ advent-of-code/2016/Day16/Puzzle.cs
@@ -0,0 +1,51 @@
+namespace advent_of_code._2016.Day16;
+
+/// <summary>
+/// Year 2016 Day 16 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2016/day/16"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        //Arrange
+        var initialState = "10000";
+        var length = 20;
+
+        //Act
+        var result = Calculations.Execute(initialState, length);
+
+        //Assert
+        Assert.AreEqual("01100", result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        //Arrange
+        var initialState = "11110010111001001";
+        var length = 272;
+
+        //Act
+        var result = Calculations.Execute(initialState, length);
+
+        //Assert
+        Assert.AreEqual("01110011101111011", result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        //Arrange
+        var initialState = "11110010111001001";
+        var length = 35651584;
+
+        //Act
+        var result = Calculations.Execute(initialState, length);
+
+        //Assert
+        Assert.AreEqual("11001111011000111", result);
+    }
+}
