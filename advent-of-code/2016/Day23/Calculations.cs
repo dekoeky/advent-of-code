aa6cf0841a851bc1d709078fa179ca0872cc3966
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-09 18:26:17 +0200

    year 2016 day 23

---
+++ advent-of-code/2016/Day23/Calculations.cs
@@ -0,0 +1,16 @@
+namespace advent_of_code._2016.Day23;
+
+internal static class Calculations
+{
+    public static int Calculate(int input) => Factorial(input) + 5621;
+
+    private static int Factorial(int n)
+    {
+        var result = 1;
+
+        for (int i = 2; i <= n; i++)
+            result *= i;
+
+        return result;
+    }
+}
