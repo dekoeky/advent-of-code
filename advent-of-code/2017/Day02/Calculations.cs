47aca57c7b39dde4a197d2a2db73068aa29e897c
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-10 14:45:47 +0200

    year 2017 day 02

---
+++ advent-of-code/2017/Day02/Calculations.cs
@@ -0,0 +1,92 @@
+namespace advent_of_code._2017.Day02;
+
+internal static class Calculations
+{
+    private static readonly char[] Separators = ['\t', ' '];
+
+    public static int Checksum1(string input)
+    {
+        var checksum = 0;
+
+        foreach (var line in input.EnumerateLines())
+        {
+            var min = int.MaxValue;
+            var max = int.MinValue;
+
+            foreach (var range in line.SplitAny(' ', '\t'))
+            {
+                var value = int.Parse(line[range]);
+
+                if (value > max) max = value;
+                if (value < min) min = value;
+            }
+
+            // the difference for this line
+            var diff = max - min;
+
+            // add this line difference to the checksum
+            checksum += diff;
+        }
+
+        return checksum;
+    }
+
+    public static int Checksum2(string input)
+    {
+        var checksum = 0;
+        Span<int> values = stackalloc int[20]; // Large enough for our example
+
+        foreach (var line in input.EnumerateLines())
+        {
+            var i = 0;
+
+            foreach (var range in line.SplitAny(' ', '\t'))
+            {
+                // Store value
+                values[i] = int.Parse(line[range]);
+
+                // Test this value against all previous ones
+                var valuesToTestAgainst = values.Slice(0, i);
+
+                // If none of the previous values is dividible by the last value (or vice versa) we keep processing values
+                if (!ContainsEvenDivision(valuesToTestAgainst, values[i], out var result))
+                {
+                    i++;
+                    continue;
+                }
+                // We found the solution for this line
+                // Add the result (the divide result) to the checksum
+                checksum += result;
+
+                // No need to check the other values on this line, there is only one set of divisible values each row
+                break;
+            }
+        }
+
+        return checksum;
+
+        static bool ContainsEvenDivision(ReadOnlySpan<int> values, int a, out int result)
+        {
+            foreach (var b in values)
+                if (b > a && b % a == 0)
+                {
+                    result = b / a;
+                    return true;
+                }
+                else if (b < a && a % b == 0)
+                {
+                    result = a / b;
+                    return true;
+                }
+                else if (a == b)
+                {
+                    result = 1;
+                    return true;
+                }
+
+            // No result found
+            result = 0;
+            return false;
+        }
+    }
+}
---
+++ advent-of-code/2017/Day02/Puzzle.cs
@@ -0,0 +1,61 @@
+namespace advent_of_code._2017.Day02;
+
+/// <summary>
+/// Year 2017 Day 02 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2017/day/2"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        // Arrange
+        var input = Inputs.Example1;
+
+        // Act
+        var result = Calculations.Checksum1(input);
+
+        // Assert
+        Assert.AreEqual(18, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Checksum1(input);
+
+        // Assert
+        Assert.AreEqual(39126, result);
+    }
+
+    [TestMethod]
+    public void Part2Example()
+    {
+        // Arrange
+        var input = Inputs.Example2;
+
+        // Act
+        var result = Calculations.Checksum2(input);
+
+        // Assert
+        Assert.AreEqual(9, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Checksum2(input);
+
+        // Assert
+        Assert.AreEqual(258, result);
+    }
+}
