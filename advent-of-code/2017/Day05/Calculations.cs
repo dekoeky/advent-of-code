347833310e642d09be1e39b68d0a2d688af69032
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-11 23:02:55 +0200

    year 2017 day 05

---
+++ advent-of-code/2017/Day05/Calculations.cs
@@ -0,0 +1,59 @@
+using advent_of_code.Helpers;
+
+namespace advent_of_code._2017.Day05;
+
+internal static class Calculations
+{
+    public static int Part1(string input)
+    {
+        var jumps = Parse(input);
+
+        int steps = 0;
+        int nextInstruction = 0;
+
+        while (nextInstruction >= 0 && nextInstruction < jumps.Count)
+        {
+            // Lookup Jump instruction
+            var jump = jumps[nextInstruction];
+
+            // Increase current instruction
+            jumps[nextInstruction]++;
+
+            // Perform jump instruction
+            nextInstruction += jump;
+            steps++;
+        }
+
+        return steps;
+    }
+
+    public static int Part2(string input)
+    {
+        var jumps = Parse(input);
+
+        int steps = 0;
+        int nextInstruction = 0;
+
+        while (nextInstruction >= 0 && nextInstruction < jumps.Count)
+        {
+            // Lookup Jump instruction
+            var jump = jumps[nextInstruction];
+
+            // Calculate the increment
+            var increment = jump >= 3
+                ? -1
+                : +1;
+
+            // Increase current instruction
+            jumps[nextInstruction] += increment;
+
+            // Perform jump instruction
+            nextInstruction += jump;
+            steps++;
+        }
+
+        return steps;
+    }
+
+    private static List<int> Parse(string input) => [.. SplitOn.NewLines(input).Select(int.Parse)];
+}
---
+++ advent-of-code/2017/Day05/Puzzle.cs
@@ -0,0 +1,61 @@
+namespace advent_of_code._2017.Day05;
+
+/// <summary>
+/// Year 2017 Day 05 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2017/day/5"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(5, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(326618, result);
+    }
+
+    [TestMethod]
+    public void Part2Example()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(10, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(21841249, result);
+    }
+}
