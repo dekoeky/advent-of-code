7acb90fa778ba0260e950d319f3d2cbb5736e2ae
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-13 20:15:41 +0200

    year 2017 day 11

---
+++ advent-of-code/2017/Day11/Calculations.cs
@@ -0,0 +1,28 @@
+namespace advent_of_code._2017.Day11;
+
+internal static class Calculations
+{
+    private static readonly Dictionary<string, HexGridCoordinate> Moves = new()
+    {
+        ["n"] = new HexGridCoordinate(0, 1, -1),
+        ["ne"] = new HexGridCoordinate(1, 0, -1),
+        ["se"] = new HexGridCoordinate(1, -1, 0),
+        ["s"] = new HexGridCoordinate(0, -1, 1),
+        ["sw"] = new HexGridCoordinate(-1, 0, 1),
+        ["nw"] = new HexGridCoordinate(-1, 1, 0),
+    };
+
+    public static int Execute(string input, out int maxDistance)
+    {
+        HexGridCoordinate position = new();
+        maxDistance = 0;
+
+        foreach (var part in input.Split(','))
+        {
+            position += Moves[part];
+            maxDistance = Math.Max(maxDistance, position.Distance());
+        }
+
+        return position.Distance();
+    }
+}
---
+++ advent-of-code/2017/Day11/HexGridCoordinate.cs
@@ -0,0 +1,9 @@
+namespace advent_of_code._2017.Day11;
+
+internal record struct HexGridCoordinate(int X, int Y, int Z)
+{
+    public static HexGridCoordinate operator +(HexGridCoordinate a, HexGridCoordinate b)
+        => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+    public readonly int Distance() => (Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z)) / 2;
+}
