615c7714831268c29db455f50e5d14c70abb3958
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-14 22:07:01 +0200

    year 2017 day 16

---
+++ advent-of-code/2017/Day16/Puzzle.cs
@@ -0,0 +1,51 @@
+namespace advent_of_code._2017.Day16;
+
+/// <summary>
+/// Year 2017 Day 16 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2017/day/16"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        // Arrange
+        var instructions = Inputs.Example;
+        var start = "abcde";
+
+        // Act
+        var result = Calculations.Part1(start, instructions);
+
+        // Assert
+        Assert.AreEqual("baedc", result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+        var start = "abcdefghijklmnop";
+
+        // Act
+        var result = Calculations.Part1(start, input);
+
+        // Assert
+        Assert.AreEqual("fgmobeaijhdpkcln", result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var instructions = Inputs.Puzzle;
+        var start = "abcdefghijklmnop";
+
+        // Act
+        var result = Calculations.Part2(start, instructions);
+
+        // Assert
+        Assert.AreEqual("lgmkacfjbopednhi", result);
+    }
+}
