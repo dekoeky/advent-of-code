305f570889e7770ccb5948f1aa9e42d42407fcf5
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-16 13:31:54 +0200

    year 2017 day 23

---
+++ advent-of-code/2017/Day23/Parser.cs
@@ -0,0 +1,36 @@
+namespace advent_of_code._2017.Day23;
+
+internal static class Parser
+{
+    public static Instruction[] Parse(ReadOnlySpan<char> input)
+    {
+        int n = input.Count('\n') + 1;
+        var instructions = new Instruction[n];
+        int idx = 0;
+
+        foreach (var line in input.EnumerateLines())
+        {
+            if (line.IsEmpty)
+                continue;
+
+            var op = line[..3];
+            var rest = line.Length > 4 ? line[4..] : [];
+            int space = rest.IndexOf(' ');
+            var p1 = space == -1 ? rest : rest[..space];
+            var p2 = space == -1 ? [] : rest[(space + 1)..];
+
+            instructions[idx++] = op switch
+            {
+                "set" => new Set(p1[0], Parameter.Parse(p2)),
+                "sub" => new Sub(p1[0], Parameter.Parse(p2)),
+                "mul" => new Mul(p1[0], Parameter.Parse(p2)),
+                "jnz" => new Jnz(Parameter.Parse(p1), Parameter.Parse(p2)),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        return instructions;
+    }
+}
+
+
---
+++ advent-of-code/2017/Day23/ProgramState.cs
@@ -0,0 +1,17 @@
+namespace advent_of_code._2017.Day23;
+
+internal sealed class ProgramState
+{
+    public Dictionary<char, long> Regs { get; }
+    public Queue<long> Q { get; } = new();
+    public int Ip;
+    public long SendCount;
+    public bool Blocked;
+    public bool Terminated;
+
+    public ProgramState(long id)
+    {
+        Regs = Enumerable.Range('a', 26).ToDictionary(i => (char)i, _ => 0L);
+        Regs['p'] = id;
+    }
+}
