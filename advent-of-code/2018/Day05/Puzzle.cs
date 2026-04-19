36205e0af7469b1ea51691d2d37e261ba550e41a
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-19 18:02:14 +0200

    year 2018 day 05

---
+++ advent-of-code/2018/Day05/Puzzle.cs
@@ -0,0 +1,61 @@
+namespace advent_of_code._2018.Day05;
+
+/// <summary>
+/// Year 2018 Day 05 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2018/day/5"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(10, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(11310, result);
+    }
+
+    [TestMethod]
+    public void Part2Example()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(4, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(6020, result);
+    }
+}
