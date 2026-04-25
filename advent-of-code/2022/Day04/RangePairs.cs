0091fb6ecec90e5bbd6f612ad43a7281b0de83c1
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-25 12:56:23 +0200

    year 2022 day 04

---
+++ advent-of-code/2022/Day04/RangePairs.cs
@@ -0,0 +1,16 @@
+namespace advent_of_code._2022.Day04;
+
+internal readonly record struct RangePairs(RangePair[] Pairs)
+{
+    public static RangePairs Parse(ReadOnlySpan<char> input)
+    {
+        var n = input.Count('\n') + 1;
+        var pairs = new RangePair[n];
+        var i = 0;
+
+        foreach (var line in input.EnumerateLines())
+            pairs[i++] = RangePair.Parse(line);
+
+        return new RangePairs(pairs);
+    }
+}
