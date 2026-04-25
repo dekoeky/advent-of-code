e8c1796b1a3f2b8e353be438f8b2b19426f835b3
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-25 11:34:26 +0200

    year 2019 day 04

---
+++ advent-of-code/2019/Day04/Inputs.cs
@@ -0,0 +1,6 @@
+namespace advent_of_code._2019.Day04;
+
+public static class Inputs
+{
+    public const string Puzzle = "197487-673251";
+}
---
+++ advent-of-code/2019/Day04/Puzzle.cs
@@ -0,0 +1,48 @@
+namespace advent_of_code._2019.Day04;
+
+/// <summary>
+/// Year 2019 Day 04 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2019/day/4"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    [DataRow(111111, true)]
+    [DataRow(223450, false)]
+    [DataRow(123789, false)]
+    public void Part1Examples(int input, bool expected)
+    {
+        // Act
+        var result = Calculations.IsValid(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(1640, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(1126, result);
+    }
+}
