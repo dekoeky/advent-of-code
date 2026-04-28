c115522dae9f6cd82ffbc59cb545e5ac149c3b3d
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-28 21:41:01 +0200

    year 2020 day 05

---
+++ advent-of-code/2020/Day05/Calculations.cs
@@ -0,0 +1,60 @@
+using advent_of_code.Helpers;
+
+namespace advent_of_code._2020.Day05;
+
+internal static partial class Calculations
+{
+    public static int Part1(ReadOnlySpan<char> input)
+    {
+        var max = int.MinValue;
+
+        foreach (var line in input.EnumerateLines())
+            max = Math.Max(max, Seat.ParseId(line));
+
+        return max;
+    }
+
+    public static int Part2(ReadOnlySpan<char> input)
+    {
+        var seats = Seat.ParseMany(input);
+
+        // Visualizing this map is how i found the solution
+        var map = SeatMap(seats);
+        PrintMap(map);
+
+        var row = seats
+            .GroupBy(s => s.Row)
+            .First(g => g.Count() == 7)
+            .Key;
+
+        var col = Enumerable.Range(0, 8)
+            .First(c => !seats.Any(s => s.Row == row && s.Column == c));
+
+        return Seat.SeatId(row, col);
+    }
+
+    private static void PrintMap(char[,] map)
+    {
+        Debug.WriteLine("    01234567");
+        for (var r = 0; r < map.GetLength(0); r++)
+        {
+            Debug.Write($"{r,3} ");
+            for (var c = 0; c < map.GetLength(1); c++)
+            {
+                Debug.Write(map[r, c]);
+            }
+            Debug.WriteLine();
+        }
+    }
+
+    private static char[,] SeatMap(Seat[] seats)
+    {
+        var map = new char[128, 8];
+        Array.Fill(map, '.');
+
+        foreach (var seat in seats)
+            map[seat.Row, seat.Column] = '#';
+
+        return map;
+    }
+}
---
+++ advent-of-code/2020/Day05/Seat.cs
@@ -0,0 +1,95 @@
+namespace advent_of_code._2020.Day05;
+
+internal readonly record struct Seat(int Row, int Column)
+{
+
+    private static readonly Range SeatRowRange = ..7;
+    private static readonly Range SeatColumnRange = 7..;
+
+    public int Id { get; init; } = SeatId(Row, Column);
+
+    public static Seat Parse(ReadOnlySpan<char> input)
+    {
+        var row = ParseRow(input[SeatRowRange]);
+        var col = ParseColumn(input[SeatColumnRange]);
+
+        return new Seat(row, col);
+    }
+
+    public static Seat[] ParseMany(ReadOnlySpan<char> input)
+    {
+        var n = input.Count('\n') + 1;
+        var i = 0;
+        var result = new Seat[n];
+
+        foreach (var line in input.EnumerateLines())
+            result[i++] = Parse(line);
+
+        return result;
+    }
+
+    public static int ParseId(ReadOnlySpan<char> input)
+    {
+        int row = ParseRow(input[SeatRowRange]);
+        var col = ParseColumn(input[SeatColumnRange]);
+        var id = SeatId(row, col);
+
+        return id;
+    }
+
+    public static int SeatId(int row, int col) => (row * 8) + col;
+
+    public static int ParseRow(ReadOnlySpan<char> input)
+    {
+        if (input.Length != 7)
+            throw new InvalidOperationException($"Input not 7 characters long ({input.Length})");
+
+        var row = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            row <<= 1;
+
+            switch (input[i])
+            {
+                case 'F':
+                    continue;
+
+                case 'B':
+                    row += 1;
+                    continue;
+
+                default: throw new InvalidOperationException();
+            }
+        }
+
+        return row;
+    }
+
+    public static int ParseColumn(ReadOnlySpan<char> input)
+    {
+        if (input.Length != 3)
+            throw new InvalidOperationException($"Input not 3 characters long ({input.Length})");
+
+        var column = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            column <<= 1;
+
+            switch (input[i])
+            {
+                case 'L':
+                    continue;
+
+                case 'R':
+                    column += 1;
+                    continue;
+
+                default: throw new InvalidOperationException();
+            }
+        }
+
+        return column;
+    }
+}
