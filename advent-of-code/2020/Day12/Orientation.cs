32b554ad9da44458276c2015cb7abe6da59e2ff2
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-29 00:17:49 +0200

    year 2020 day 12

---
+++ advent-of-code/2020/Day12/Orientation.cs
@@ -0,0 +1,9 @@
+namespace advent_of_code._2020.Day12;
+
+internal enum Orientation
+{
+    N = 0,
+    E = 90,
+    S = 180,
+    W = 270,
+};
---
+++ advent-of-code/2020/Day12/Position.cs
@@ -0,0 +1,3 @@
+namespace advent_of_code._2020.Day12;
+
+internal readonly record struct Position(int x, int y);
