215460c802d515ab7d6ce7596261344238182fca
Wesley Van Koeckhoven <wvk@outlook.be>
2026-04-30 12:42:45 +0200

    year 2021 day 07

---
+++ advent-of-code/Helpers/CommaSeparatedNumbers.cs
@@ -0,0 +1,22 @@
+using System.Numerics;
+
+namespace advent_of_code.Helpers;
+
+/// <summary>
+/// Parses comma-separated numbers from strings.
+/// </summary>
+internal static class CommaSeparatedNumbers
+{
+    public static T[] Parse<T>(ReadOnlySpan<char> input) where T : struct, INumberBase<T>
+    {
+        var n = input.Count(',') + 1;
+        var i = 0;
+
+        T[] values = new T[n];
+
+        foreach (var r in input.Split(','))
+            values[i++] = T.Parse(input[r], null);
+
+        return values;
+    }
+}
