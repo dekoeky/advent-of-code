4733b0290e2f1a2c64fc253b6b8280f60dda48ac
Wesley Van Koeckhoven <wvk@outlook.be>
2026-05-03 20:07:59 +0200

    year 2022 day 11

---
+++ advent-of-code/2022/Day11/Puzzle.cs
@@ -0,0 +1,61 @@
+namespace advent_of_code._2022.Day11;
+
+/// <summary>
+/// Year 2020 Day 11 solution.
+/// </summary>
+/// <seealso href="https://adventofcode.com/2020/day/11"/>
+[TestClass]
+public class Puzzle
+{
+    [TestMethod]
+    public void Part1Example()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(105 * 101, result);
+    }
+
+    [TestMethod]
+    public void Part1Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part1(input);
+
+        // Assert
+        Assert.AreEqual(316888, result);
+    }
+
+    [TestMethod]
+    public void Part2Example()
+    {
+        // Arrange
+        var input = Inputs.Example;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(52166L * 52013L, result);
+    }
+
+    [TestMethod]
+    public void Part2Puzzle()
+    {
+        // Arrange
+        var input = Inputs.Puzzle;
+
+        // Act
+        var result = Calculations.Part2(input);
+
+        // Assert
+        Assert.AreEqual(35270398814, result);
+    }
+}
